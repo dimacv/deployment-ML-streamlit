5643eab68a3825386675420b121dacf79d70698e
Dmitriy Tsvirkun <cvirkun@gmail.com>
2023-02-12 15:56:40 +0300

    v1

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 # импортируем библиотеки pandas, numpy
 import pandas as pd
 import numpy as np
-
 # импортируем библиотеку streamlit
 import streamlit as st
 # импортируем модуль pickle
 import pickle
-
-# загрузим из CSV-файлов тестовые данные для проверки
-#X_test = pd.read_csv('X_test.csv')
-#y_test = pd.read_csv('y_test.csv')
 
 # загрузим модель стандартизации
 with open('deployment_standardscaler.pkl', 'rb') as f:
@@ -17,22 +12,6 @@ with open('deployment_standardscaler.pkl', 'rb') as f:
 # загрузим модель logreg
 with open('deployment_logreg.pkl', 'rb') as f:
     logreg = pickle.load(f)
-
-# применяем модель стандартизации к тестовому массиву признаков
-#X_test_standardscaled = standardscaler.transform(X_test)
-
-# оцениваем качество модели на тестовых данных
-#print("Правильность на тестовой выборке: {:.3f}".format(
-#    logreg.score(X_test_standardscaled, y_test)))
-
-# вычисляем спрогнозированные значения зависимой переменной
-# для тестового массива признаков
-#logreg_predvalues = logreg.predict(X_test_standardscaled)
-#logreg_predvalues
-
-
-
-
 
 ###  Функция запуска web интерфейса
 def run():
@@ -48,7 +27,6 @@ def run():
     ("Online", "Batch"))
 
     st.sidebar.info('Прогнозирования отклика на предложение автостраховки с использованием логистической регрессии')
-    #st.sidebar.success('https://github.com/Gewissta/Data_Preprocessing_in_Python/blob/main/code/2_7.02._%D0%A1%D1%82%D1%80%D0%BE%D0%B8%D0%BC%20%D1%81%D0%B2%D0%BE%D0%B9%20%D0%BF%D0%B5%D1%80%D0%B2%D1%8B%D0%B9%20%D0%BA%D0%BE%D0%BD%D0%B2%D0%B5%D0%B9%D0%B5%D1%80%20%D0%BC%D0%BE%D0%B4%D0%B5%D0%BB%D0%B5%D0%B9.ipynb')
 
     st.title("Прогнозирования отклика на предложение автостраховки:")
 
@@ -81,11 +59,11 @@ def run():
             output = logreg.predict(input_df_standardscaled)
             output = str(output)
 
-        st.success('The output is {}'.format(output))
+        st.success('Прогноз: {}'.format(output))
 
     if add_selectbox == 'Batch':
 
-        file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+        file_upload = st.file_uploader("Загрузите csv-файл с данными для осуществления прогнозов по ним:", type=["csv"])
 
         if file_upload is not None:
             data = pd.read_csv(file_upload)
